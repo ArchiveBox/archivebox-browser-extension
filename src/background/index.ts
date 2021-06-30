9a5b8b530698dce05995c21dc86592065b64ec74
TJ Horner <me@tjhorner.com>
2021-06-30 10:48:41 -0400

    New context menu item

--- src/background/index.ts
+++ src/background/index.ts
@@ -2,13 +2,17 @@ import ArchiveBoxArchiver from "../common/services/archiver"
 import SyncedConfig from "../common/services/config"
 import ChromeSyncStorage from "../common/services/storage"
 import DomainList from "../common/services/domainList"
+import IArchiver from "../common/interfaces/archiver"
+import { GlobalConfigKey } from "../common/interfaces/config"
+import { v4 } from "uuid"
+import { ListType } from "../common/interfaces/domainList"
 
 async function main() {
   const storage = new ChromeSyncStorage()
   const config = new SyncedConfig(storage)
   const domainList = new DomainList(storage)
 
-  const archiver = new ArchiveBoxArchiver(domainList, config)
+  const archiver: IArchiver = new ArchiveBoxArchiver(domainList, config)
 
   chrome.history.onVisited.addListener(async historyItem => {
     const shouldArchive = await archiver.shouldArchive(historyItem.url)
@@ -33,6 +37,22 @@ async function main() {
     contexts: [ "all" ],
     onclick: async (info) => {
       await archiver.archiveImmediately(info.pageUrl)
+    }
+  })
+
+  chrome.contextMenus.create({
+    id: "addDomain",
+    title: "Add Current Domain to List",
+    contexts: [ "all" ],
+    onclick: async (info) => {
+      const currentList = await config.get(GlobalConfigKey.ArchiveMode, "allowlist")
+
+      const url = new URL(info.pageUrl)
+      await domainList.addEntry({
+        id: v4(),
+        type: "domain",
+        value: url.host
+      }, currentList as ListType)
     }
   })
 
