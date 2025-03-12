02f356522c47c9724d5e5e09fb9ca88697d95b26
Nick Sweeting <github@sweeting.me>
2025-03-11 23:28:45 -0700

    fix permission requesting inside extension

--- utils.js
+++ utils.js
@@ -18,14 +18,6 @@ export function filterEntries(entries, filterText) {
 }
 
 export async function addToArchiveBox(addCommandArgs, onComplete, onError) {
-  // request permission to POST to the ArchiveBox server, upgrade optional_host_permissions
-  // const permission = await chrome.permissions.request({permissions: [archivebox_server_url, 'host_permissions']});
-  const permission = await chrome.permissions.request({origins: [`${archivebox_server_url}/*`]});
-  if (!permission) {
-    onError({ok: false, errorMessage: 'Permission denied.'});
-    return;
-  }
-
   try {
     const { archivebox_server_url, archivebox_api_key } = await new Promise((resolve, reject) => {
       const vals = chrome.storage.local.get([
@@ -42,6 +34,14 @@ export async function addToArchiveBox(addCommandArgs, onComplete, onError) {
 
     if (!archivebox_server_url) {
       throw new Error('Server not configured.');
+    }
+
+    // request permission to POST to the ArchiveBox server, upgrade optional_host_permissions
+    // const permission = await chrome.permissions.request({permissions: [archivebox_server_url, 'host_permissions']});
+    const permission = await chrome.permissions.request({origins: [`${archivebox_server_url}/*`]});
+    if (!permission) {
+      onError({ok: false, errorMessage: 'Permission denied.'});
+      return;
     }
 
     let response = undefined;
